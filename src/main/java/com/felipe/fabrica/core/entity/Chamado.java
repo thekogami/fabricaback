878c7e804391d6c78d2a6727bea41aee7071aba6
Felipe Mourao <felipe.mourao@sensustec.com.br>
2024-12-02 22:08:16 -0300

    new

--- src/main/java/com/felipe/fabrica/core/entity/Chamado.java
+++ src/main/java/com/felipe/fabrica/core/entity/Chamado.java
@@ -4,10 +4,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -21,9 +24,15 @@ public class Chamado {
 
     private String descricao;
     private String status;
+    private LocalDateTime dataAbertura;
 
     public Chamado(String descricao, String status) {
         this.descricao = descricao;
         this.status = status;
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        this.dataAbertura = LocalDateTime.now();
     }
 }
